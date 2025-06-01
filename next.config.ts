b1b3066931f5b99ad5302ff58fc138dff557d61f
miscode <nmamadoundir@gmail.com>
2025-06-01 18:19:27 +0200

    nouveau commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
   typescript: {
     // ❌ Ignore les erreurs TypeScript au build
     ignoreBuildErrors: true,
