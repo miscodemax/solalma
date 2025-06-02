2832ad5b4f5dbb8e28ef433824580478289b15fc
miscode <nmamadoundir@gmail.com>
2025-06-02 12:46:22 +0200

    change marque

--- app/composants/textLogo.tsx
+++ app/composants/textLogo.tsx
@@ -3,7 +3,7 @@
 export default function TextLogo() {
     return (
         <h2 className="text-4xl font-bold text-red-400 italic tracking-wider font-serif">
-            Jayma
+            Solalma
         </h2>
 
     )
