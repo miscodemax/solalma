0f93003d2d12676250fe86638accf9272d19e772
miscode <nmamadoundir@gmail.com>
2025-06-06 17:14:13 +0200

    head

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/sangse.png" type="image/png" sizes="32x32" />
+      {/* Tu peux aussi ajouter d'autres meta tags ici */}
+    </>
+  )
+}
