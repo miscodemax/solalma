f34a3f098ad2a98882aff05f5a4be5e118707188
miscode <nmamadoundir@gmail.com>
2025-09-23 16:15:17 +0200

    test redeploy

--- app/composants/textLogo.tsx
+++ app/composants/textLogo.tsx
@@ -1,7 +1,7 @@
 export default function TextLogo() {
     return (
         <h2 className="text-3xl font-bold text-[#F4B400] tracking-wide font-sans">
-            Sangsé
+            SangséShop
         </h2>
     )
 }
