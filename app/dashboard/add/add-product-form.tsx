6cb7a2a285f922f5cc448893ee757ddebb9dd96a
mamadou ndir ndiaye <nmamadoundir@gmail.com>
2026-03-22 16:48:34 +0000

    hrh

--- app/dashboard/add/add-product-form.tsx
+++ app/dashboard/add/add-product-form.tsx
@@ -673,7 +673,7 @@ export default function AddProductForm({ userId }: Props) {
         <div
           style={{
             position: "fixed",
-            bottom: 20,
+            bottom: 40,
             left: 0,
             right: 0,
             zIndex: 9999,
